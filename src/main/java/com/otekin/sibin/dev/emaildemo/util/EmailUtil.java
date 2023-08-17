125891fe02aa988d7750e785505b10ea56251b40
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-08-18 01:12:07 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/sibin/dev/emaildemo/util/EmailUtil.java
@@ -0,0 +1,29 @@
+package com.otekin.sibin.dev.emaildemo.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+@Component
+public class EmailUtil {
+	@Autowired
+	private JavaMailSender javaMailSender;
+	private static final Logger LOG = LoggerFactory.getLogger(EmailUtil.class);
+
+	public void sendEmail() throws MessagingException, IOException {
+		LOG.info("Ready to sent an email");
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo("knowledgefactory4upeoples@gmail.com");
+		msg.setSubject("Test Mail");
+		msg.setText("Greetings from Spring Boot Email");
+		javaMailSender.send(msg);
+		LOG.info("Successfully dropped an email");
+
+	}
+}
